2342912b3f73aa2955045eef909d535421aff7fa
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-06-27 09:10:29 +0200

    .

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,18 @@
-Menu
+<div class="menu">Menu</div> 
 <br>
 <div class="content"><slot /></div>
 <style>
     :global(body) {
         margin: 0;
-        background-color: aquamarine;
+        background-color: rgb(73, 90, 84);
         text-align: center;
     }
     div.content {
-        padding: 20px;background-color: beige;
+        padding: 20px;
+    }
+    div.menu {
+        background-color: black;
+        color: white;
+        padding: 10px;
     }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,19 @@
 <script>
     export let form, data
-    console.log(form, data)
 </script>
-Cica {data.x} <br><br>
+<h1>Cica {data.x}</h1>
+
 <form method=POST>
     <input type="text" name="x">
     <input type="submit">
 </form>
+
+<h2>{Number(form?.y || 0) * 2}</h2>
+
 <style lang="scss">
+    h1, h2 {
+        color: aliceblue;
+    }
     form {
         input {
             border: 1px solid rgb(119, 95, 95);
