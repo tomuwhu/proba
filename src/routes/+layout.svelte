03f80a4e58e18e0f767d665aeb4aa0173a23737d
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-06-27 12:23:53 +0200

    .

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,18 +2,18 @@
 div(class="menu") Menu 
 br
 div(class="content") 
-    slot
+  slot
 </template>
+
 <style lang="sass">
 :global(body) 
-    margin: 0
-    background-color: rgb(73, 90, 84)
-    text-align: center
+  margin: 0
+  background-color: rgb(73, 90, 84)
+  text-align: center
 div.content 
-    padding: 20px
+  padding: 20px
 div.menu 
-    background-color: black
-    color: white
-    padding: 10px
-    
+  background-color: black
+  color: white
+  padding: 10px
 </style>
